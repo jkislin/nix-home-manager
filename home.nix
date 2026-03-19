e1db665d6a6586f0d656b24579a8ad5faccfc8dd
jkislin <qxk3@cdc.gov>
2026-03-19 22:30:27 +0000

    Set a default .Rprofile that will enable default install of R binaries under the hood

--- home.nix
+++ home.nix
@@ -78,4 +78,44 @@
         azure-cli
         azure-storage-azcopy
       ];
+
+      # Set ~/.Rprofile for out-of-the-box ubuntu R package binaries. 
+      # Thanks to Zack Susswein for the code!
+      home.file.".Rprofile".text = ''
+        # Set default user agent header
+        options(HTTPUserAgent = sprintf(
+          "R/%s R (%s)", 
+          getRversion(), 
+          paste(getRversion(), 
+          R.version["platform"], 
+          R.version["arch"], 
+          R.version["os"]))
+        )
+
+        # Also use this user agent header for wget and curl from within R
+        options(download.file.extra = sprintf(
+          "--header \"User-Agent: R (%s)\"", 
+          paste(getRversion(), 
+          R.version["platform"], 
+          R.version["arch"], 
+          R.version["os"]))
+        )
+
+        LINUX_VERSION = system("grep VERSION_CODENAME /etc/os-release | cut -d '=' -f2", intern = TRUE)
+
+        options(
+          repos = c(
+            CRAN = sprintf(
+              "https://packagemanager.rstudio.com/all/__linux__/%s/latest", 
+              LINUX_VERSION
+            ), 
+            getOption("repos")
+          )
+        )
+
+        rm(LINUX_VERSION)
+        
+        system('echo "nix home-manager .Rprofile for user $USER loaded succesfully" | lolcat')
+        cat("\n")
+      '';
 }
